8c4931fbe1c7489947ea3fd09fcd3f3bfd455adf
Odos Matthews <odosmatthews@Odoss-MacBook-Pro.local>
2022-08-21 09:31:07 -0400

    version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="tinytable",
-    version="0.4",
+    version="0.3",
     description="Pure Python lite version of Pandas DataFrame.",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -23,5 +23,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     python_requires='>=3.6',
-    install_requires=['tinytim', 'openpyxl', 'tabulate']
+    install_requires=['openpyxl', 'tabulate']
 )
