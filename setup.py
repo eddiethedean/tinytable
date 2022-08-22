80230b02d99ae0515f08d018d4bb5e93c90de1eb
Odos Matthews <odosmatthews@odoss-mac-mini.lan>
2022-08-22 12:14:59 -0400

    changed dependency to python 3.7

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="tinytable",
-    version="0.3",
+    version="0.5.1",
     description="Pure Python lite version of Pandas DataFrame.",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -22,6 +22,6 @@ setup(
     ],
     packages=find_packages(),
     include_package_data=True,
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=['tinytim', 'openpyxl', 'tabulate']
 )
