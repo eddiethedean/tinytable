c3908355f67cdb952eed3b8e8344d0e50a5e8dd3
Odos Matthews <odosmatthews@odoss-mac-mini.lan>
2022-08-25 15:29:27 -0400

    add sqlite column type converter

--- tinytable/sqlite.py
+++ tinytable/sqlite.py
@@ -1,7 +1,8 @@
 import sqlite3
-from typing import List
+from typing import List, Mapping, MutableMapping, Optional
 
 from tinytable.functional.utils import combine_names_rows
+from tinytable.functional.copy import copy_table
 
 
 def _table_names(conn) -> List[str]:
@@ -31,3 +32,57 @@ def read_sqlite_table(path: str, table_name: str) -> dict:
         column_names = _table_column_names(con, table_name)
         rows = _select_all_rows(con, table_name)
     return combine_names_rows(column_names, rows)
+
+
+def sqlite_type(column: list) -> str:
+    """
+    Check what sqlite type column of values can be converted to.
+    
+    Returns Sqlite type name.
+    
+    """
+    if all(isinstance(x, int) for x in column):
+        return 'INTEGER'
+    if all(isinstance(x, (int, float)) for x in column):
+        return 'REAL'
+    if all(isinstance(x, bytes) for x in column):
+        return 'BLOB'
+    if all(isinstance(x, str) for x in column):
+        return 'TEXT'
+    else:
+        return 'OBJECT'
+    
+
+def sqlite_column_types(data: Mapping) -> dict:
+    """
+    Check what sqlite types each column can be converted to.
+    
+    Return dict[column_name, sqlite_type_name]
+    """
+    return {name: sqlite_type(values) for name, values in data.items()}
+
+
+def data_to_sqlite_table(
+    data: MutableMapping,
+    path: str,
+    table_name: str,
+    data_types: Optional[dict] = None) -> None:
+    """
+    Create Sqlite Table and insert data.
+    Error if table_name already exists.
+    Guesses sqlite data types for columns if not provided.
+    """
+    data = copy_table(data)
+    if data_types is None:
+        data_types = sqlite_column_types(data)
+
+    # Convert any OBJECT data types to str
+    for name, type in data_types.items():
+        if type == 'OBJECT':
+            data[name] = [str(x) for x in data[name]]
+        data_types[name] = 'TEXT'
+
+    # Create Sqlite Table with column types
+
+    # Insert data rows
+
