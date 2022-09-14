748fa6ff56e9e13869e9bafc548aa854191201d1
Odos Matthews <odosmatthews@odoss-mac-mini.lan>
2022-09-14 11:11:25 -0400

    version bumb

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="tinytable",
-    version="0.10.1",
+    version="0.11.0",
     description="Pure Python lite version of Pandas DataFrame.",
     long_description=README,
     long_description_content_type="text/markdown",
