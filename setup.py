d98e9c66cffae62c3f409b547c22ef9d11eb6bdf
Odos Matthews <odosmatthews@odoss-mac-mini.lan>
2022-10-24 16:00:40 -0400

    fix version requirements

--- setup.py
+++ setup.py
@@ -23,5 +23,5 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     python_requires='>=3.7',
-    install_requires=['tinytim>=1.7.0', 'openpyxl', 'tabulate', 'sqlite-utils']
+    install_requires=['tinytim>=1.7.1', 'openpyxl', 'tabulate', 'sqlite-utils']
 )
